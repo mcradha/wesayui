e3ca7e90ba6a3078a3cba26f6923d05119340699
Subhradip Bose <subhradip.bose@gmailc.om>
2018-05-12 16:52:50 +0530

    first commit

--- index.php
+++ index.php
@@ -8,8 +8,22 @@ else
 	$sessionid = $_SESSION['token'];
 	switch($_GET['q']){
 		case "welcome":
-		include_once("themes/pages/index.html");
+			include_once("themes/templates/header.php");
+			include_once("themes/pages/index.html");
 		break;
+		case "settings":
+			include_once("themes/templates/header.php");
+			include_once("themes/pages/forms.html");
+		break;
+		case "contacts":
+			include_once("themes/templates/header.php");
+			include_once("themes/pages/contacts.html");
+		break;
+		case "friends":
+			include_once("themes/templates/header.php");
+			include_once("themes/pages/friends.html");
+		break;
+		
 		case "logout":
 		 session_destroy();
 		 header("Location: index.php");
--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,6 @@
-var endpointurl ="http://ec2-18-219-80-120.us-east-2.compute.amazonaws.com:8080";
+//var endpointurl ="http://ec2-18-219-80-120.us-east-2.compute.amazonaws.com:8080";
+var endpointurl ="http://localhost:8080";
+ 
 function login() {
 	 
 	 var data = { "emailaddress": $("#txtusername").val(), "password": $("#txtpassword").val() } ;
@@ -22,6 +24,74 @@ function login() {
 });
 }
 
+function getMySettings(authorizationToken) {
+	 
+	 var data = { } ;
+	 $.ajax({
+    type: "POST",
+	beforeSend: function(request) {
+    request.setRequestHeader("X-Authorization", authorizationToken);
+   },
+     url: endpointurl+"/userzone/mysettings/",
+    
+    data: JSON.stringify(data),
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: function(data){
+		$('#mytraits').html(data.length);
+		 var positive = 0;
+		 var negetive = 0;
+		 var neutral = 0;
+		var trHTML = '';
+        $.each(data, function (i, item) {
+			var checked = "";
+			if(item.categoryvalue==1)
+			{
+				checked = "checked";
+			}
+			var func = "applySettings(this,'"+authorizationToken+"')";
+			
+			trHTML += '<div class="checkbox"><label><input onclick="'+func+'" type="checkbox"  '+ checked +' value="'+item.uniqueid+'">'+ item.categoryname +'</label></div>';
+            
+		});
+        $('#settings').append(trHTML);
+		  
+		},
+    failure: function(errMsg) {
+        alert(errMsg);
+    }
+});
+}
+function applySettings(sid, authorizationToken){
+	if(sid.checked){
+		var data = { "uniqueid": sid.value, "categoryvalue": 1 } ;
+	}
+	else
+	{
+		var data = { "uniqueid": sid.value, "categoryvalue": 0 } ;
+	}
+ $.ajax({
+    type: "POST",
+	beforeSend: function(request) {
+    request.setRequestHeader("X-Authorization", authorizationToken);
+   },
+     url: endpointurl+"/userzone/changemysettings/",
+    
+    data: JSON.stringify(data),
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: function(data){
+		 
+         window.location.href = window.location.href; 
+		  
+		},
+    failure: function(errMsg) {
+        alert(errMsg);
+    }
+});
+	
+	}
+
 function getMyTraits(authorizationToken) {
 	 
 	 var data = { } ;
@@ -36,11 +106,30 @@ function getMyTraits(authorizationToken) {
     contentType: "application/json; charset=utf-8",
     dataType: "json",
     success: function(data){
+		$('#mytraits').html(data.length);
+		 var positive = 0;
+		 var negetive = 0;
+		 var neutral = 0;
 		var trHTML = '';
         $.each(data, function (i, item) {
 			var func = "deleteTrait('"+authorizationToken+"','"+item.traituniqid+"')";
-            trHTML += '<tr><td>' + (i +1) + '</td><td>' + item.traitname + '</td><td><span style="cursor:pointer" onclick='+func+' class="glyphicon glyphicon-remove"></span></td></tr>';
-        });
+			var func1 = "";
+			
+			var func1class = "";
+			if(parseInt(item.ishidden) == 0 ) {
+				 
+				func1 = "ChangeStatusOfTrait('"+authorizationToken+"','"+item.traituniqid+"',0)";
+				func1class = "glyphicon glyphicon-remove";
+			}
+			else 
+			{
+				 
+				func1 = "ChangeStatusOfTrait('"+authorizationToken+"','"+item.traituniqid+"',1)";
+				func1class = "glyphicon glyphicon-ok";
+			}    
+            trHTML += '<tr><td>' + (i +1) + '</td><td>' + item.traitname + '</td><td><span style="cursor:pointer" onclick='+func+' class="glyphicon glyphicon-trash"></span></td><td><span style="cursor:pointer" onclick='+func1+' class=" '+func1class+'"></span></td></tr>';
+			positive = positive + item.positive ;
+		});
         $('#records_table').append(trHTML);
 		  
 		},
@@ -50,6 +139,193 @@ function getMyTraits(authorizationToken) {
 });
 }
 
+function sendInvite(friendId){
+	var data = [{ "friendsid": friendId }] ;
+	 $.ajax({
+    type: "POST",
+     url: endpointurl+"/userzone/sendfriendrequest/",
+    beforeSend: function(request) {
+    request.setRequestHeader("X-Authorization", authorizationToken);
+   },
+    data: JSON.stringify(data),
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: function(data){
+		 
+		},
+    failure: function(errMsg) {
+        alert(errMsg);
+    }
+});
+}
+function getMyContacts() {
+	 
+	 var data = { } ;
+	 $.ajax({
+    type: "POST",
+	beforeSend: function(request) {
+    request.setRequestHeader("X-Authorization", authorizationToken);
+   },
+     url: endpointurl+"/userzone/getMyContacts/",
+    
+    data: JSON.stringify(data),
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: function(data){
+		var trHTML  = "";
+		var func = "";
+		 $.each(data, function (i, item) {
+			 if(parseInt(i, item.isregistredinwesay)==0){
+				trHTML += '<tr><td>' + (i +1) + '</td><td>' + item.fullname + '</td><td>' + item.emailaddress + '</td><td>' + item.countrycode +"-"+item.mobilenumber + '</td><td><span style="cursor:pointer"   class="glyphicon glyphicon-envelope"></span></td></tr>';
+			 }
+			 else
+			 {
+				 func = "sendInvite('"+item.contactid+"')";
+				 trHTML += '<tr><td>' + (i +1) + '</td><td>' + item.fullname + '</td><td>' + item.emailaddress + '</td><td>' + item.countrycode +"-"+item.mobilenumber + '</td><td><span style="cursor:pointer" onclick="'+func+'"  class="glyphicon glyphicon-heart"></span></td></tr>';
+			 }
+		});
+        $('#records_table').append(trHTML);
+		  
+		},
+    failure: function(errMsg) {
+        alert(errMsg);
+    }
+});
+}
+function getMyFriendRequest() {
+	 
+	 var data = { } ;
+	 $.ajax({
+    type: "POST",
+	beforeSend: function(request) {
+    request.setRequestHeader("X-Authorization", authorizationToken);
+   },
+     url: endpointurl+"/userzone/checkfriendrequest/",
+    
+    data: JSON.stringify(data),
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: function(data){
+		var trHTML  = "";
+		var func = "";
+		 
+		 
+        $('#friendrequest').html(data.friend_request);
+		  
+		},
+    failure: function(errMsg) {
+        alert(errMsg);
+    }
+});
+}
+
+
+function getMyFriends(){
+	
+ 
+	var data = {} ;
+	 $.ajax({
+    type: "POST",
+	beforeSend: function(request) {
+    request.setRequestHeader("X-Authorization", authorizationToken);
+   },
+     url: endpointurl+"/userzone/myFriends/",
+    
+    data: JSON.stringify(data),
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: function(data){
+		 var trHTML = '';
+        $.each(data, function (i, item) {
+			var func = "addTrait('"+authorizationToken+"','"+item.traitname+"')";
+            trHTML += '<tr><td>' + (i +1) + '</td><td>' + item.traitname + '</td><td><span style="cursor:pointer" onclick='+func+' class="glyphicon glyphicon-plus"></span></td></tr>';
+        });
+        $('#records_table2').append(trHTML);
+		},
+    failure: function(errMsg) {
+        alert(errMsg);
+    }
+});
+ 
+}
+function saveConatct()
+{
+	var data = [{ "emailaddress" : $("#txemail").val(),"countrycode" : $("#txtcountrycode").val(),"mobilenumber" : $("#txtmobilenumber").val(),"fullname" : $("#txtname").val()}] ;
+	 $.ajax({
+    type: "POST",
+	beforeSend: function(request) {
+    request.setRequestHeader("X-Authorization", authorizationToken);
+   },
+     url: endpointurl+"/userzone/addContacts/",
+    
+    data: JSON.stringify(data),
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: function(data){
+		  window.location.href = window.location.href; 
+		  
+		},
+    failure: function(errMsg) {
+        alert(errMsg);
+    }
+});
+}
+
+function ChangeStatusOfTrait(authorizationToken,traituniqid, statusValue ){
+	var ePoint = "";
+	if(statusValue ==0) {
+		ePoint = "/traitapi/hideTrait/";
+	}
+	else
+	{
+		ePoint = "/traitapi/unhideTrait/";
+	}
+	  
+	var data = { "traituniqueid" : traituniqid} ;
+	 $.ajax({
+    type: "POST",
+	beforeSend: function(request) {
+    request.setRequestHeader("X-Authorization", authorizationToken);
+   },
+	 
+     url: endpointurl+ePoint,
+    
+    data: JSON.stringify(data),
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: function(data){
+		window.location.href = window.location.href; 
+		  
+		},
+    failure: function(errMsg) {
+        alert(errMsg);
+    }
+});
+}
+
+
+function addCustomTrait(authorizationToken) {
+	 
+	 var data = [{ "traitname" : $("#txtCustomTrait").val(),"traitgivenfor" : "0"}] ;
+	 $.ajax({
+    type: "POST",
+	beforeSend: function(request) {
+    request.setRequestHeader("X-Authorization", authorizationToken);
+   },
+     url: endpointurl+"/traitapi/addTrait/",
+    
+    data: JSON.stringify(data),
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: function(data){
+		  window.location.href = window.location.href; 
+		  
+		},
+    failure: function(errMsg) {
+        alert(errMsg);
+    }
+});
+}
 function getPopuler(authorizationToken) {
 	 
 	 var data = { } ;
@@ -94,7 +370,7 @@ if(c) {
     dataType: "json",
     success: function(data){
 		 
-		 window.location.href = window.location.href;
+		 window.location.href = "index.php?q=welcome";
 		},
     failure: function(errMsg) {
         alert(errMsg);
